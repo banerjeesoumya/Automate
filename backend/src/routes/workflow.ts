81b21be56b4500b4065296d377b2ab6f4838a511
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2025-10-23 23:04:16 +0530

    Added routes for fetching all workflows and particular workflow

--- backend/src/routes/workflow.ts
+++ backend/src/routes/workflow.ts
@@ -54,3 +54,77 @@ workflowRouter.post('/workflows', authMiddleware(), async (c) => {
     }
 })
 
+const getOneWorkflowSchema = z.object({
+    id: z.string()
+})
+ 
+workflowRouter.get('/workflows/all', authMiddleware(), async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.CONNECTION_POOL_URL
+    }).$extends(withAccelerate());
+    const userId = c.get('userId');
+    // console.log("Hello from workflows/all endpoint");
+    if (!userId) {
+        return c.json({ message: 'User not logged in' }, 401);
+    }
+    try {
+        const workflows = await prisma.workflow.findMany({
+            where: {
+                userId: userId
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        })
+        console.log(workflows)
+        return c.json({
+            ok: true,
+            workflows: workflows,
+            message: 'Workflows fetched successfully',
+        })
+    } catch (error) {
+        console.error('Error fetching workflows:', error);
+        return c.json({
+            ok: false,
+            message: 'Error fetching workflows',
+        }, 500);
+    }
+})
+
+workflowRouter.get('/workflows/:id', authMiddleware(), async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.CONNECTION_POOL_URL
+    }).$extends(withAccelerate());
+    const userId = c.get('userId');
+    // const body = await c.req.json();
+    // const parseResult = getOneWorkflowSchema.safeParse(body);
+    const parseResult = getOneWorkflowSchema.safeParse({
+        id: c.req.param('id')
+    });
+    if (!parseResult.success) {
+        return c.json({ message: 'Invalid request', errors: parseResult.error.errors }, 400);
+    }
+    console.log('Fetching workflows for user:', userId);
+    if (!userId) {
+        return c.json({ message: 'User not logged in' }, 401);
+    }
+    try {
+        const workflow = await prisma.workflow.findUnique({
+            where: {
+                id: parseResult.data.id,
+                userId: userId
+            },
+        })
+        return c.json({
+            ok: true,
+            workflow: workflow,
+            message: 'Workflow fetched successfully',
+        })
+    } catch (error) {
+        console.error('Error fetching workflows:', error);
+        return c.json({
+            ok: false,
+            message: 'Error fetching workflows',
+        }, 500);
+    }
+})
