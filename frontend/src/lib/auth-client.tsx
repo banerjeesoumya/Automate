c04e067a4b9a6e8a410a17bddb0b6ce36f97dbc1
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2025-10-27 15:11:38 +0530

    changed to production backend url

--- frontend/src/lib/auth-client.tsx
+++ frontend/src/lib/auth-client.tsx
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-    baseURL: "http://127.0.0.1:8787/api/auth",
+    baseURL: "https://backend.banerjeerik03.workers.dev/api/auth",
     fetchOptions: {
         credentials: "include"
     }
